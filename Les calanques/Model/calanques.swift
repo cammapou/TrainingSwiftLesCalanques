b375e85eef237b0940f755460fe7151cec23667c
camille <cmapoula@gmail.com>
2020-01-09 17:21:03 +0100

    add new controller

---
+++ Les calanques/Model/calanques.swift
@@ -0,0 +1,39 @@
+//
+//  calanques.swift
+//  Les calanques
+//
+//  Created by Ddales on 09/01/2020.
+//  Copyright © 2020 Ddales. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class Calanque {
+    
+    private var _nom: String
+    private var _desc: String
+    private var _latitude: Double
+    private var _longitude: Double
+    private var _image: UIImage?
+    
+    var nom: String {
+        return _nom
+    }
+    
+    var desc: String {
+        return _desc
+    }
+    
+    var coordonnee: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: _latitude, longitude: _longitude)
+    }
+    
+    init(nom: String, desc: String, image: UIImage?, latitude: Double, longitude: Double) {
+        _nom = nom
+        _desc = desc
+        _latitude = latitude
+        _longitude = longitude
+        _image = image
+    }
+}
